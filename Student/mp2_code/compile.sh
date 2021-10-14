d4d1d1c308d87e23fe85c9dd6c1ddce9249b9a10
Alvin Yang <alvin.yang68@gmail.com>
2021-10-15 12:34:13 +1300

    Remove docker compilation script
    Compile using g++ in WSL instead

--- Student/mp2_code/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker build -t cs435_mp2 .
-docker run --name cs435_mp2_compiled cs435_mp2
-docker cp cs435_mp2_compiled:/usr/src/cs435_mp2/ls_router .
-docker rm cs435_mp2_compiled
-docker rmi cs435_mp2
